6e6ac95764a393c450e771fd858e73411d628689
Paul <paul.messina@gmail.com>
2021-08-26 17:33:08 -0300

    1er try to deploy

---
+++ index.js
@@ -0,0 +1,12 @@
+/**
+ * Responds to any HTTP request.
+ *
+ * @param {!express:Request} req HTTP request context.
+ * @param {!express:Response} res HTTP response context.
+ */
+ exports.helloWorld = (req, res) => {
+    console.log("requestHelloSudamerica");
+    let message = req.query.message || req.body.message || 'Hello from San Pablo!';
+    res.status(200).send(message);
+  };
+  
